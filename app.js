28751760025ea30b152539661d8046e610ad64c3
Juan Pablo <jpcorvo219@gmail.com>
2023-11-23 13:28:19 -0300

    Pauta 2 terminada
    Co-Authored-By: Florencia <140855501+floestimi@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
 const express = require('express');
-
+const jwt = require('jsonwebtoken');
 const app = express();
-
 const port = 3000;
+const key = 'clave'
 
-app.use(express.json())
+app.use(express.json());
 
 const rutaCategorias = require('./routes/rutaCategorias');
 const rutaCatProductos = require('./routes/rutaCatProductos');
 const rutaProducto = require('./routes/rutaProducto');
 const rutaComentarios = require('./routes/rutaComentarios');
 const rutaCarrito = require('./routes/rutaCarrito');
+const usuarios = require('./jsons/users/users.json');
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
     next();
   });
-  
 
+app.post("/login", (req, res) => {
+
+ const {usuario, contraseña} = req.body;
+    
+  if (!usuario || !contraseña){
+        return res.status(401).json({msj: 'Falta usuario o contraseña'});
+    }
+
+    const usuarioExiste = usuarios.find(user=> user.usuario ===usuario && user.contraseña===contraseña);
+    if (!usuarioExiste){
+        return res.status(401).json({msj:'Usuario o clave no válidos'});
+    }
+  
+    const token = jwt.sign({userId: usuario.id, username:usuarioExiste.usuarios},key/*,{expiresIn: Math.floor(Date.now()/100)+10}*/);
+    res.token = token;
+    res.send(token);
+});
 
 app.listen(port, () =>{
     console.log("Bienvenido a nuestro potentismo servidor lleno de errores, corriendo en el puerto: " + port )
@@ -26,7 +44,7 @@ app.listen(port, () =>{
 
 app.get('/', (req,res) => {
     res.send("<h1>Servidor</h1>")
-})
+});
 
 app.use("/categorias", rutaCategorias);
 
